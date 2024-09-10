8cad9be101062024b6aea1520531f26ca5af8635
hugo12345678999 <vendpromax@gmail.com>
2024-09-10 18:12:07 -0300

    Initial commit

--- js/roleta.js
+++ js/roleta.js
@@ -1,5 +1,5 @@
 const items = [
-    ['Item 2', 90], // Peso alto para garantir maior chance de ser escolhido
+    ['Item 8', 90], // Peso alto para garantir maior chance de ser escolhido
     ['Item 3', 10],
     ['Item 5', 10],
 ];
