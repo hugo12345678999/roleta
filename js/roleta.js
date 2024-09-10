4749ebb4eb3ac12599f3871c760385ee12817e6a
hugo12345678999 <vendpromax@gmail.com>
2024-09-10 13:10:56 -0300

    Initial commit

--- js/roleta.js
+++ js/roleta.js
@@ -1,16 +1,29 @@
+$(document).ready(function() {
+    // Requisição GET quando a página é carregada
+    $.get('https://roletahugo-b69e04045a85.herokuapp.com/valores', function(data) {
+        console.log('Dados recebidos:', data);
+        // Aqui você pode processar os dados recebidos e atualizar o array `items`
+        // Exemplo de atualização, assumindo que a resposta seja um array de itens
+        // items = data; // Atualize conforme a estrutura da resposta
+    }).fail(function() {
+        console.error('Erro ao carregar dados da API.');
+    });
+
+    // Evento de clique na div-roulette
+    $('.div-roulette').on('click', function() { rodaARoda(); });
+
+    // Evento de tecla pressionada
+    $(document).keypress(function(event) {
+        if (event.charCode == 13 || event.charCode == 32) {
+            rodaARoda();
+        }
+    });
+});
 const items = [
     ['Item 7', 10], // Peso alto para garantir maior chance de ser escolhido
     ['Item 14', 10],
     ['Item 17', 10],
 ];
-
-$('.div-roulette').on('click', function() { rodaARoda(); });
-
-$(document).keypress(function(event) {
-    if (event.charCode == 13 || event.charCode == 32) {
-        rodaARoda();
-    }
-});
 
 
 function rodaARoda() {
